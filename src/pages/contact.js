f13a4c36f61a697e414b87c88c5b9cb39f017b78
Sam Ilkov <samuil.ilkov90@gmail.com>
2022-07-09 23:13:28 -0400

    fixing form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,37 +1,77 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from '../components/layout'
 
-const Contact = () => (
-    <Layout>
+const Contact = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleInput = (e) => {
+        e.preventDefault();
+        const name = e?.target?.name;
+        const value = e?.target?.value;
+
+        if (name === 'name') {
+            setName(value);
+        }
+        if (name === 'email') {
+            setEmail(value);
+        }
+        if (name === 'subject') {
+            setSubject(value);
+        }
+        if (name === 'message') {
+            setMessage(value);
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const state = {
+            name: name,
+            email: email,
+            subject: subject,
+            message: message
+        }
+        fetch('/', {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...state})
+          })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+    };
+    
+    return <Layout>
         <h2 className="contact-heading">Get in Touch</h2>
         <p className="contact-desc">Questions, comments, jobs, projects, whatever. Give me a shout.</p>
         <div className="contact-form">
-        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={(e) => handleSubmit(e)}>
         <input type="hidden" name="sams-form" value="contact" />
                 <label>
                     Name
-                <input type="text" name="name" id="name" />
+                <input type="text" name="name" id="name" onChange={(e) => handleInput(e)}/>
                 </label>
                 <label>
                     Email
-                <input type="email" name="email" id="email" />
+                <input type="email" name="email" id="email" onChange={(e) => handleInput(e)}/>
                 </label>
                 <label>
                     Subject
-                <input type="text" name="subject" id="subject" />
+                <input type="text" name="subject" id="subject" onChange={(e) => handleInput(e)}/>
                 </label>
                 <label>
                     Message
-                <textarea name="message" id="message" rows="5" />
+                <textarea name="message" id="message" rows="5" onChange={(e) => handleInput(e)} />
                 </label>
                 <div className="form-actions">
-                    {/* <input type="reset" id="clear" value="Clear" /> */}
                     <button type="submit">Submit</button>
                 </div>
             </form>
         </div>
     </Layout>
-
-)
+}
 
 export default Contact
